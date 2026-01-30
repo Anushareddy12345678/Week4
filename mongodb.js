593671cef3e5c8816aec4112792078a23c1db2e5
Anureddy97 <anureddy972005@gmail.com>
2026-01-30 13:09:58 +0530

    Add files via upload

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://localhost:27017/ANU")
+.then(()=>{
+    console.log("Mongo db connected")
+})
+.catch(()=>{
+    console.log("failed")
+})
+
+const LoginSchema=new mongoose.Schema({
+    name:String,
+    password:String,
+})
+
+const collection=new mongoose.model("CSMD19",LoginSchema)
+module.exports=collection
+
+
+
